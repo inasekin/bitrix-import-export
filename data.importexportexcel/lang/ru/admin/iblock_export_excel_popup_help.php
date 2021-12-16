92f479cb2dfeb00273c5c8491abdc55a9bb79d51
Ivan Nasekin <nasekin@ipavlov.ai>
2021-12-16 18:02:28 +0300

    initial commit

---
+++ data.importexportexcel/lang/ru/admin/iblock_export_excel_popup_help.php
@@ -0,0 +1,13 @@
+<?
+/**
+ * Copyright (c) 5/3/2021 Created By/Edited By nasekinid nasekinid8591@yandex.ru
+ */
+
+$MESS["KDA_EE_HELP_TAB1"] = "Видео-инструкции";
+$MESS["KDA_EE_HELP_TAB1_ALT"] = "Видео-инструкции по настройке экспорта";
+$MESS["KDA_EE_HELP_TAB2"] = "FAQ";
+$MESS["KDA_EE_HELP_TAB2_ALT"] = "Вопросы и ответы по работе модуля";
+$MESS["KDA_EE_HELP_VIDEO_COMMON"] = "Основная инструкция по экспорту";
+$MESS["KDA_EE_HELP_VIDEO_PRICELIST"] = "Создание прайс-листа с автообновлением";
+$MESS["KDA_EE_HELP_VIDEO_FILTERING"] = "Настройка фильтрации выгружаемых данных";
+?>
---
+++ data.importexportexcel/lang/ru/install/install.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright (c) 5/3/2021 Created By/Edited By nasekinid nasekinid8591@yandex.ru
+ */
+
+$MESS ['DATA_IMPORTEXPORTEXCEL_MODULE_NAME'] = 'Экспорт/Импорт товаров в Excel';
+$MESS ['DATA_IMPORTEXPORTEXCEL_MODULE_DESCRIPTION'] = 'Экспорт/Импорт элементов и разделов инфоблока в Excel-файлы';
+$MESS ['DATA_PARTNER_NAME'] = 'NASEKINID';
+?>
